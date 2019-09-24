223b36a1152e612db0e9de507adcd0282e23e651
venture21 <park0122@gmail.com>
2019-09-24 11:46:49 +0900

    init commit

---
+++ src/main/java/kr/co/itcen/mysite/dao/UserDao.java
@@ -0,0 +1,139 @@
+package kr.co.itcen.mysite.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import kr.co.itcen.mysite.vo.UserVo;
+
+public class UserDao {
+	
+	public Boolean insert(UserVo vo) {
+		Boolean result = false;
+		
+		Connection connection = null;
+		PreparedStatement pstmt = null;
+		
+		Statement stmt = null;
+		ResultSet rs = null;
+		
+		try {
+			connection = getConnection();
+			
+			String sql = "insert into user values(null, ?, ?, ?, ?, now())";
+			pstmt = connection.prepareStatement(sql);
+			pstmt.setString(1, vo.getName());
+			pstmt.setString(2, vo.getEmail());
+			pstmt.setString(3, vo.getPassword());
+			pstmt.setString(4, vo.getGender());
+			int count = pstmt.executeUpdate();
+			result = (count == 1);
+			
+			stmt = connection.createStatement();
+			rs = stmt.executeQuery("select last_insert_id()");
+			if(rs.next()) {
+				Long no = rs.getLong(1);
+				vo.setNo(no);
+			}
+			
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		} finally {
+			try {
+				if(rs != null) {
+					rs.close();
+				}
+				if(stmt != null) {
+					stmt.close();
+				}
+				
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				
+				if(connection != null) {
+					connection.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return result;		
+	}
+
+
+	
+
+
+	
+	public UserVo get(Long no) {
+		return null;
+	}
+	
+	public UserVo get(String email, String password) {
+		UserVo result = null;
+		
+		Connection connection = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			connection = getConnection();
+			
+			String sql = "select no, name from user where email = ? and password = ?";
+			pstmt = connection.prepareStatement(sql);
+			pstmt.setString(1, email);
+			pstmt.setString(2, password);
+			
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				Long no = rs.getLong(1);
+				String name = rs.getString(2);
+				
+				result = new UserVo();
+				result.setNo(no);
+				result.setName(name);
+			}
+			
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		} finally {
+			try {
+				if(rs != null) {
+					rs.close();
+				}
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				if(connection != null) {
+					connection.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return result;		
+	}
+	
+	private Connection getConnection() throws SQLException {
+		Connection connection = null;
+		
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+		
+			String url = "jdbc:mariadb://192.168.1.62:3306/webdb?characterEncoding=utf8";
+			connection = DriverManager.getConnection(url, "webdb", "webdb");
+		
+		} catch (ClassNotFoundException e) {
+			System.out.println("Fail to Loading Driver:" + e);
+		}
+		
+		return connection;
+	}
+
+}
