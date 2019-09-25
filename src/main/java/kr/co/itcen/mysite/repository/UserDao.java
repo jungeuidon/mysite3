0c64442091253f6c39f539e3160e2de01eb3fa6e
venture21 <park0122@gmail.com>
2019-09-25 14:51:56 +0900

    mybatis #1

--- src/main/java/kr/co/itcen/mysite/repository/UserDao.java
+++ src/main/java/kr/co/itcen/mysite/repository/UserDao.java
@@ -1,14 +1,11 @@
 package kr.co.itcen.mysite.repository;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -19,60 +16,11 @@ import kr.co.itcen.mysite.vo.UserVo;
 public class UserDao {
 	
 	@Autowired
-	private DataSource dataSource;
+	private SqlSession sqlSession;
 	
 	public Boolean insert(UserVo vo) throws UserDaoException{
-		Boolean result = false;
-		
-		Connection connection = null;
-		PreparedStatement pstmt = null;
-		
-		Statement stmt = null;
-		ResultSet rs = null;
-		
-		try {
-			connection = dataSource.getConnection();
-			
-			String sql = "insert into user values(null, ?, ?, ?, ?, now())";
-			pstmt = connection.prepareStatement(sql);
-			pstmt.setString(1, vo.getName());
-			pstmt.setString(2, vo.getEmail());
-			pstmt.setString(3, vo.getPassword());
-			pstmt.setString(4, vo.getGender());
-			int count = pstmt.executeUpdate();
-			result = (count == 1);
-			
-			stmt = connection.createStatement();
-			rs = stmt.executeQuery("select last_insert_id()");
-			if(rs.next()) {
-				Long no = rs.getLong(1);
-				vo.setNo(no);
-			}
-			
-		} catch (SQLException e) {
-			throw new UserDaoException(e.getMessage());
-		} finally {
-			try {
-				if(rs != null) {
-					rs.close();
-				}
-				if(stmt != null) {
-					stmt.close();
-				}
-				
-				if(pstmt != null) {
-					pstmt.close();
-				}
-				
-				if(connection != null) {
-					connection.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		
-		return result;		
+		int count = sqlSession.insert("user.insert", vo);
+		return count == 1;		
 	}
 	
 	public UserVo get(Long no) {
@@ -80,52 +28,16 @@ public class UserDao {
 	}
 	
 	public UserVo get(UserVo vo) {
-		return get(vo.getEmail(), vo.getPassword());
+		UserVo result = sqlSession.selectOne("user.getByEmailAndPassword1", vo);
+		return result; 
 	}
 	
 	public UserVo get(String email, String password) {
-		UserVo result = null;
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("email", email);
+		map.put("password", password);
 		
-		Connection connection = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		
-		try {
-			connection = dataSource.getConnection();
-			
-			String sql = "select no, name from user where email = ? and password = ?";
-			pstmt = connection.prepareStatement(sql);
-			pstmt.setString(1, email);
-			pstmt.setString(2, password);
-			
-			rs = pstmt.executeQuery();
-			if(rs.next()) {
-				Long no = rs.getLong(1);
-				String name = rs.getString(2);
-				
-				result = new UserVo();
-				result.setNo(no);
-				result.setName(name);
-			}
-			
-		} catch (SQLException e) {
-			System.out.println("error:" + e);
-		} finally {
-			try {
-				if(rs != null) {
-					rs.close();
-				}
-				if(pstmt != null) {
-					pstmt.close();
-				}
-				if(connection != null) {
-					connection.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		
+		UserVo result = sqlSession.selectOne("user.getByEmailAndPassword2", map);
 		return result;		
 	}
 }
